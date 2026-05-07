8f37d3a4d4b4576c40cc147a27e66de975de8205
likith007156 <likithkumar156@example.com>
2026-05-07 11:02:59 +0530

    Simplify middleware to fix deployment error

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,9 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   locales: ['hi-IN', 'kn-IN', 'ta-IN', 'te-IN', 'mr-IN', 'en-IN'],
-  defaultLocale: 'en-IN',
-  localePrefix: 'always'
+  defaultLocale: 'en-IN'
 });
  
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|_vercel|favicon.ico).*)',
-  ]
+  matcher: ['/', '/(hi-IN|kn-IN|ta-IN|te-IN|mr-IN|en-IN)/:path*']
 };
