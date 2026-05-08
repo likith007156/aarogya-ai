f321f527551897dddbc7c8aab038f5498143dc4b
likith007156 <likithkumar156@example.com>
2026-05-08 17:30:04 +0530

    Fix middleware matcher regex for Vercel Edge

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
+import {routing} from './src/i18n/routing';
 
-export default createMiddleware({
-  locales: ['en-IN', 'hi-IN', 'kn-IN', 'ta-IN', 'te-IN', 'mr-IN'],
-  defaultLocale: 'en-IN',
-  localePrefix: 'as-needed'
-});
+export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)']  
+  matcher: [
+    '/',
+    '/(hi-IN|kn-IN|ta-IN|te-IN|mr-IN|en-IN)/:path*'
+  ]
 };
